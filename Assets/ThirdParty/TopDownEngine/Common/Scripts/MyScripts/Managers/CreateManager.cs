7951f3384ff1b0ca438a1cb5fabcd84ff879e91f
sangkyu156 <84694606+sangkyu156@users.noreply.github.com>
2023-10-06 01:19:32 +0900

    일하면서 깔짝3
    등변사다리꼴 범위안에 바리게이트 돌맹이1 생성 완료

--- Assets/ThirdParty/TopDownEngine/Common/Scripts/MyScripts/Managers/CreateManager.cs
+++ Assets/ThirdParty/TopDownEngine/Common/Scripts/MyScripts/Managers/CreateManager.cs
@@ -9,6 +9,7 @@ namespace MoreMountains.TopDownEngine
     public class CreateManager : MonoBehaviour
     {
         PoolManager poolManager; //오브젝트 풀링 매니져
+        GameObject level; //Battlefield 씬 맵
 
         static CreateManager c_instance; // 유일성이 보장된다       
         static public CreateManager Instance { get { /*Init();*/ return c_instance; } } // 유일한 매니저를 갖고온다
@@ -23,7 +24,21 @@ namespace MoreMountains.TopDownEngine
 
         void Start()
         {
+            level = GameObject.FindWithTag("Level");
 
+            for (int i = 0;i < 2000;i++)
+            {
+                BarricadeRock_1();
+            }
+
+            if (GameManager.Instance.scenes == Define.Scenes.Battlefield)
+            {
+                level = GameObject.FindWithTag("Level");
+
+                BarricadeRockCreation();
+
+                
+            }
         }
 
         private void Update()
@@ -39,7 +54,26 @@ namespace MoreMountains.TopDownEngine
         }
 
         //바리게이트 돌멩이 프리펩 0~4개 랜덤으로 생성
+        void BarricadeRockCreation()
+        {
+            BarricadeRock_1();
 
+            //돌 생성후 네비매쉬 굽기
+        }
+
+        void BarricadeRock_1()
+        {
+            //1번 돌맹이 생성 범위(x축을 -7,-8, ... , -31, -32 으로 정하고 그에따라 z축도 결정해준다.)
+            //1.x축이 - 32일때 z축은 -32~32 까지
+            //2.x축이 - 31일떄 z축은 -31~31 까지
+            //...
+            //마지막.축이 - 7일때 z축은 -7~7 까지
+            int posX = Random.Range(-7, -33);
+            int posZ = Random.Range(posX, -posX);
+
+            GameObject barricadeRock_1 = Instantiate("Battlefield/BF_BarricadeRock1", level.transform);
+            barricadeRock_1.transform.position = new Vector3(posX, 0, posZ);
+        }
 
         static public T Load<T>(string path) where T : Object
         {
